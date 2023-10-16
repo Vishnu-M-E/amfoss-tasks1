e26ca519a80aca60af1d2b9e9cf7fe102ce1921f
Vishnu M <vishnu240404@gmail.com>
2023-10-16 23:16:54 +0530

    updates

--- task-06/bot.py
+++ task-06/bot.py
@@ -12,7 +12,7 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client)) #necessary?
 
 @client.event
 async def on_message(message):
