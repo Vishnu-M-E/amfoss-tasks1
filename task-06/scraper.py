7064964937846b55c2e3de08a00e09475ca3a463
Vishnu M <vishnu240404@gmail.com>
2023-10-17 16:39:20 +0530

    updates

--- task-06/scraper.py
+++ task-06/scraper.py
@@ -6,10 +6,8 @@ def scrape_data():
     url = 'https://www.espncricinfo.com/live-cricket-score'
     response = requests.get(url)
     
-    # Parse the HTML content of the webpage
     soup = BeautifulSoup(response.content, 'html.parser')
-    
-    # Extract the team names, scores, and summary of the match
+
     all=soup.find("div",class_="ds-text-compact-xxs").text
     team1 = soup.find("p",class_="ds-text-tight-m ds-font-bold ds-capitalize ds-truncate").text    
     team2 = soup.find_all("p",class_="ds-text-tight-m ds-font-bold ds-capitalize ds-truncate")[1].text
@@ -23,11 +21,9 @@ def scrape_data():
 
     result=soup.find("p",class_="class=ds-text-tight-s ds-font-regular ds-truncate ds-text-typo")
    
-    # Get the current timestamp
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         
-    # Append the scraped data and the timestamp to a CSV file
     with open('scores.csv', 'a') as file:
-        file.write(f'{team1},{over1},{score1},{team2},{over2},{score2},{result} {timestamp}\n')
+        file.write(f'{team1},{over1},{score1},{team2},{over2},{score2},{result},{timestamp}\n')
 
 
